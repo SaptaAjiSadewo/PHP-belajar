a1e1affe512357d3b57f2db963566320d5340be6
Sapta Aji Sadewo <147994368+SaptaAjiSadewo@users.noreply.github.com>
2025-10-15 15:20:35 +0700

    index.php

---
+++ include/index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+  Menyalin konten dari file (php, html, text) dan memasukannya kedalam file php yang dipilih.
+  Bagian dari website kita menjadi reusable
+  Diganti hanya ketika dibutuhkan di satu tempat
+*/
+
+
+?>
