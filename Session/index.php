743d7096ab8d923027ff1f7395164e17cae0fe7a
Sapta Aji Sadewo <147994368+SaptaAjiSadewo@users.noreply.github.com>
2025-10-15 15:45:13 +0700

    index.php

---
+++ Session/index.php
@@ -0,0 +1,8 @@
+<?php
+/*
+  Session Super Global Variable yang digunakan untuk menyimpan informasi dari user untuk digunakan 
+  di beberapa halaman lainnya.
+  User akan memasukkan session-id
+  seperti login-credentials
+*/
+
