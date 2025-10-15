1c722b19a77a0c708b82d801c8d1567edfd5a27d
Sapta Aji Sadewo <147994368+SaptaAjiSadewo@users.noreply.github.com>
2025-10-15 16:47:11 +0700

    index.php

--- KoneksiMySQL/index.php
+++ KoneksiMySQL/index.php
@@ -1,5 +1,29 @@
 <?php
   include("database.php");
+
+  $username = "Botol";
+  $password = "admin2";
+  $hashed_password = ($password, PASSWORD_DEFAULT);
+
+  // Menyimpan Query SQL di dalam variabel $sql
+  $sql = "INSERT INTO users (user, password)
+          VALUES ('$username', '$hashed_password')";
+          //VALUES ('Sapta', 'admin')";
+
+try {  
+/*
+    Gunakan mysqli_query untuk mengirimkan query
+    Argumen Pertama adalah Koneksi Database
+    Argumen Kedua adalah Query SQL
+  */
+  mysqli_query($koneksi, $sql);
+  echo "User sekarang sudah register";
+} 
+catch (mysqli_sql_exception) {
+  echo "Tidak bisa melakukan register"
+}
+
+  mysqli_close($koneksi);
 ?>
 
 <!DOCTYPE html>
