b397d101e959cd39db45d3362540f448ffbd90bc
Sapta Aji Sadewo <147994368+SaptaAjiSadewo@users.noreply.github.com>
2025-10-15 15:48:56 +0700

    index.php 2

--- Session/index.php
+++ Session/index.php
@@ -6,3 +6,27 @@
   seperti login-credentials
 */
 
+// Membuat session
+  session_start();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+  <body>
+    Ini adalah Halaman Login
+    <a href="home.php">Ini akan menuju ke Halaman Login</a>
+  </body>
+</html>
+
+<?php
+  $_SESSION["username"] = "Sapta";
+  $_SESSION["password"] = "admin";
+
+  echo $_SESSION["username"] . "<br>";
+  echo $_SESSION["password"] . "<br>";
+?>
+
