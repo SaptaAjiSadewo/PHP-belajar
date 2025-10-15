a198d98b8d2e260270bdf4dc7eb05735ce3558fd
Sapta Aji Sadewo <147994368+SaptaAjiSadewo@users.noreply.github.com>
2025-10-15 15:51:35 +0700

    home.php 1

--- Session/home.php
+++ Session/home.php
@@ -1,1 +1,20 @@
+<?php
+  session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+  <body>
+    Ini adalah Halaman Home
+    <a href="index.php">Ini akan menuju ke Halaman Login</a>
+  </body>
+</html>
+
+<?php
+  echo $_SESSION["username"] . "<br>";
+  echo $_SESSION["password"] . "<br>";
+?>
 
