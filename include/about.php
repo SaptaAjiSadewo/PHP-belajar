3fa3c9c844e5df36821bde9a00e3296d00a0d85d
Sapta Aji Sadewo <147994368+SaptaAjiSadewo@users.noreply.github.com>
2025-10-15 15:29:55 +0700

    about.php 2

--- include/about.php
+++ include/about.php
@@ -1,1 +1,17 @@
+<?php
+  include("header.html");
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Page Title</title>
+</head>
+<body>
+  Ini adalah About Page loh yah<br>
+</body>
+</html>
+
+<?php
+  include("footer.html");
+?>
