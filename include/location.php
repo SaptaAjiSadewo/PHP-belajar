af8ef7deaf1d169390146fdfb701a8a608dd5012
Sapta Aji Sadewo <147994368+SaptaAjiSadewo@users.noreply.github.com>
2025-10-15 15:30:29 +0700

    location.php 2

--- include/location.php
+++ include/location.php
@@ -1,1 +1,17 @@
+<?php
+  include("header.html");
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Page Title</title>
+</head>
+<body>
+  Ini adalah Location Page loh yah<br>
+</body>
+</html>
+
+<?php
+  include("footer.html");
+?>
